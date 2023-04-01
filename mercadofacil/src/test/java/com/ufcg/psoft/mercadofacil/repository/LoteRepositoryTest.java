021a2beeb1da953bfbbbda4fc9660664e597207d
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2023-04-01 19:25:32 -0300

    Create 2 test cases to test LoteRepository save method

---
+++ mercadofacil/src/test/java/com/ufcg/psoft/mercadofacil/repository/LoteRepositoryTest.java
@@ -0,0 +1,90 @@
+package com.ufcg.psoft.mercadofacil.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.ufcg.psoft.mercadofacil.model.Lote;
+import com.ufcg.psoft.mercadofacil.model.Produto;
+
+@SpringBootTest
+@DisplayName("Testes do repositório de lotes")
+class LoteRepositoryTest {
+
+    @Autowired
+    LoteRepository<Lote, Long> driver;
+
+    Lote lote;
+
+    Lote resultado;
+
+    Produto produto;
+
+    @BeforeEach
+    void setUp() {
+        produto = Produto.builder()
+                .id(1L)
+                .nome("Produto Base")
+                .codigoBarra("123456789")
+                .fabricante("Fabricante Base")
+                .preco(100.00)
+                .build();
+        lote = Lote.builder()
+                .id(1L)
+                .numeroDeItens(100)
+                .produto(produto)
+                .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        produto = null;
+        lote = null;
+    }
+
+    @Test
+    @DisplayName("Inserir o primeiro lote de produtos no banco de dados")
+    void inserirPrimeiroLoteNoBD() {
+        // Arrange
+        driver.deleteAll();
+        // Act
+        resultado = driver.save(lote);
+        // Assert
+        assertNotNull(resultado);
+        assertEquals(1, driver.findAll().size());
+        assertEquals(lote.getId().longValue(), resultado.getId().longValue());
+        assertEquals(produto, resultado.getProduto());
+    }
+
+    @Test
+    @DisplayName("Inserir o segundo ou posterior lote de produtos no BD")
+    void inserirSegundoOuPosteriorLoteDeProdutosNoBD() {
+        // Arrange
+        driver.deleteAll();
+        Produto produto2 = Produto.builder()
+                .id(2L)
+                .nome("Produto Dois")
+                .codigoBarra("987654321")
+                .fabricante("Fabricante Dois")
+                .preco(200.00)
+                .build();
+        Lote lote2 = lote.builder()
+                .id(2L)
+                .numeroDeItens(200)
+                .produto(produto2)
+                .build();
+        driver.save(lote);
+        // Act
+        Lote resultado = driver.save(lote2);
+        // Assert
+        assertNotNull(resultado);
+        assertEquals(2, driver.findAll().size());
+        assertEquals(lote2.getId().longValue(), resultado.getId().longValue());
+        assertEquals(produto2, resultado.getProduto());
+    }
+
+}
